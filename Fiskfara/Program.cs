480673268e779ecdac9af6d4bc5e26e52b384e99
Vilius <axelvilius.ovander@elev.ga.ntig.se>
2021-04-22 17:36:01 +0200

    är lite knasig med hur den garanterar rätt svar

--- Fiskfara/Program.cs
+++ Fiskfara/Program.cs
@@ -5,11 +5,96 @@ namespace blackjack
 {
     class Program
     {
+
+
+
         static void Main(string[] args)
         {
+            intro();
+
+            Console.WriteLine(@"Tryck på ""Enter""");
+            string svar1 = Console.ReadLine();
+
+            senario1();
+            svar1 = Console.ReadLine();
+            while (svar1 != "1" || svar1 != "2" || svar1 != "3")
+            {
+                svar1 = Console.ReadLine();
+                Console.WriteLine("Skriv in ett av alternativen");
+            }
+
+            int time = 0;
+            int shelter = 0; // 1 = tent 2 = hammock
+            int heat = 0;
+            //int hp = 100;
+
+
+            svar1 = Console.ReadLine();
+            if (svar1 == "1")
+            {
+                Console.WriteLine("Du sammlar pinnar i en närliggande skog och drivträ från stranden, samt palmblad från marken.");
+                Console.WriteLine("Hur vill du att ditt skydd ska se ut.");
+                Console.WriteLine("1: Standard tält");
+                Console.WriteLine(@"   __ ");
+                Console.WriteLine(@"  /  \");
+                Console.WriteLine(@" /____\");
+                Console.WriteLine();
+                Console.WriteLine("2: Hammock");
+                Console.WriteLine(@" |\__/|");
+                Console.WriteLine(@" |    |");
+
+                while (svar1 == "1" || svar1 == "2")
+                {
+                    Console.WriteLine("Skriv in alternativen");
+                    svar1 = Console.ReadLine();
+                }
+
+                svar1 = Console.ReadLine();
+                if (svar1 == "1")
+                {
+
+                    Console.WriteLine("Du gör ett Standard tält");
+                    time++;
+                    shelter = 1;
+                    heat = heat + 10;
+
+                }
+                else if (svar1 == "2")
+                {
+                    Console.WriteLine("Du gör en Hammock");
+                    time++;
+                    shelter = 2;
+                    heat = heat + 0;
+
+                }
+                Console.WriteLine(shelter);
+                Console.ReadLine();
+            }
+            else if (svar1 == "2")
+            {
+                Console.WriteLine("Du gör upp en eld med trä du hittar i närheten. ");
+                time++;
+                heat = heat + 20;
+
+            }
+            else if (svar1 == "3")
+            {
+                Console.WriteLine("Du hittar ett par bär buskar som du inte känner igen. ");
+
+            }
 
 
 
+            Console.Clear();
+
+            Console.ReadLine();
+
+
+
+
+        }
+        static void intro()
+        {
             Console.WriteLine("");
             Console.WriteLine("");
             Console.WriteLine(@"                    ______ _     _     ______              ");
@@ -34,7 +119,6 @@ namespace blackjack
                 if (svar == "start")
                 {
                     Console.Clear();
-                    Console.WriteLine("ballt");
                     gameScene++;
 
                 }
@@ -51,10 +135,10 @@ namespace blackjack
 
                 }
             }
-            string svar1 = Console.ReadLine();
-            //int time = 0;
+        }
 
-
+        static void senario1()
+        {
             Console.WriteLine("Du är strandsatt på en ö i Karibiska havet.");
             Console.WriteLine("Du ser lite skumma fiskar, men tänker inte mer på det.");
             Console.WriteLine("Du måste förbereda för att överleva natten");
@@ -62,23 +146,6 @@ namespace blackjack
             Console.WriteLine("1: Sätt upp skydd");
             Console.WriteLine("2: Sätt upp en eld");
             Console.WriteLine("3: Hitta mat");
-
-            while (svar1 != "1" || svar1 != "2" || svar1 != "3")
-            {
-                Console.WriteLine("Skriv in alternativen");
-            }
-
-            if (svar1 == "1")
-            {
-                Console.WriteLine("");
-            }
-
-
-            Console.ReadLine();
-
-
-
-
         }
     }
 }
