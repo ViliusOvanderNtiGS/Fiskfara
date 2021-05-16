901fded91d1ef4d10738a9cf8d1b7d7515db0b06
Vilius <axelvilius.ovander@elev.ga.ntig.se>
2021-05-16 22:33:01 +0200

    har gjort klart striden

--- Fiskfara/Program.cs
+++ Fiskfara/Program.cs
@@ -14,8 +14,8 @@ namespace FiskFara
             intro();
             //gör en lista med alla opt i och visa bara relevanta opts för frågan. sen ta bort opten som är vald.
 
-            List<string> choices = new List<string>() { "1: Sätt upp skydd", "2: Sätt upp en eld", "3: Hitta mat" };
-            List<string> options = new List<string>() { "1", "2", "3" };
+            List<string> choices = new List<string>() { "1: Sätt upp skydd", "2: Sätt upp en eld", "3: Hitta mat" };// det som man gör
+            List<string> options = new List<string>() { "1", "2", "3" }; //det som man skriver
 
             //choices = new List<string>() { "1: Sätt upp skydd", "2: Sätt upp en eld", "3: Hitta mat" };
 
@@ -31,12 +31,12 @@ namespace FiskFara
                 WriteLine("Du är strandsatt på en ö i Karibiska havet.");
                 WriteLine("Du ser lite skumma fiskar, men tänker inte mer på det.");
                 WriteLine("Du måste förbereda för att överleva natten");
-                // string fråga 2
+
                 scenario(@"Vad gör du? Skriv antingen ""1"", ""2"" eller ""3""", choices);
 
                 string svar1 = Console.ReadLine();
 
-                while (!options.Contains(svar1))
+                while (!options.Contains(svar1))    // detta gör så att man bara kan skriva de svar man har
                 {
                     svar1 = Console.ReadLine();
                     WriteLine("Skriv in ett av alternativen");
@@ -59,13 +59,13 @@ namespace FiskFara
                     WriteLine(@"           |    |");
                     WriteLine();
 
+                    svar1 = Console.ReadLine();
                     while (svar1 != "1" && svar1 != "2")
                     {
                         svar1 = Console.ReadLine();
                         Console.WriteLine("Skriv in ett av alternativen");
                     }
 
-                    svar1 = Console.ReadLine();
                     if (svar1 == "1")
                     {
 
@@ -180,33 +180,79 @@ namespace FiskFara
                     WriteLine("Skriv in ett av alternativen");
                 }
 
+                Random generator = new Random();
+                int fiskattack = generator.Next(1, 100);
 
-                if (svar1 == "1") //Första frågan
+                if (svar1 == "1")
                 {
                     WriteLine("Du sitte lungt.");
                     WriteLine("Fiskarna börjar springa mot dig.");
                     WriteLine("Du blir nertacklad av dem från alla håll.");
+                    WriteLine("Du skakar undan dom och springer ifrån dem.");
                     WriteLine("De skadar dig rijält");
                     hp = hp - 50;
                     ReadLine();
                 }
+                else if (svar1 == "2")
+                {
+                    WriteLine("Du tar en pinne och viftar mot fiskarnas riktning.");
+                    WriteLine("Du träffar en fisk och den flyger bort några meter bort.");
+                    WriteLine("Du träffar några fler och de börjar dra sig tillbaka mot vattnet");
+                    if (fiskattack >= 49)
+                    {
+                        WriteLine("Du strider tappert och kommer undan oskadd.");
+                    }
+                    else
+                    {
+                        WriteLine("Du slogs väl men tar lite skada under striden.");
+                        hp = hp - 10;
+                    }
+                    ReadLine();
+                }
+                else if (svar1 == "3")
+                {
+                    string shelterS = "";
+                    if (shelter == 1)
+                    {
+                        shelterS = "ditt tält";
+                    }
+                    else if (shelter == 2)
+                    {
+                        shelterS = "din hammock";
+                    }
+
+                    WriteLine("Du springer mot " + shelterS + " för att ta skydd.");
+                    if (shelter == 1)
+                    {
+                        WriteLine("Fiskarana springer mot tältet.");
+                        WriteLine("De flockas vid öppningen.");
+                        WriteLine("Du ligger på rygg och sparkar bort dem.");
+                        WriteLine("Efter en stund så slutar de deras attak.");
+                        WriteLine("Du tog en del skada från detta.");
+                        hp = hp - 30;
+                        ReadLine();
+
+                    }
+                    else if (shelter == 2)
+                    {
+                        WriteLine("Fiskarna kan inte ta sig upp till dig.");
+                        WriteLine("Du hör schattrandet av deras tänder.");
+                        WriteLine("Efter en stund så slutar schattrandet och fiskarna drar sig tillbaka.");
+                        ReadLine();
+                    }
 
 
+                }
+            }
 
-
-
-
-
-
-
-
-
+            while (true) // samma sak som dag 1 fast med 4 alternativ och olika options
+            {
 
             }
 
 
         }
-        static void intro()
+        static void intro() // detta är intro saken till spelet. den visar titlen och man måste skriva start.
         {
             WriteLine("");
             WriteLine("");
@@ -221,7 +267,7 @@ namespace FiskFara
             WriteLine("");
             WriteLine("");
             WriteLine(@"                          Skriv ""start"" för att starta.");
-            WriteLine(@"                       Skriv ""quit"" för att göra motsattsen.");
+
 
 
             int gameScene = 0;
@@ -235,12 +281,6 @@ namespace FiskFara
                     gameScene++;
 
                 }
-                else if (svar == "quit")
-                {
-                    Console.Clear();
-                    Console.WriteLine("lol");
-                    gameScene = 999;
-                }
                 else
                 {
                     Console.WriteLine(@"Du måste skriva något antingen ""quit"" eller ""start"".");
@@ -251,7 +291,8 @@ namespace FiskFara
         }
 
 
-        static void scenario(string frågan, List<string> options) // array till att ha så många options jag vill
+        static void scenario(string frågan, List<string> options) //  detta gör så att jag kan ha så många options jag vill tilll ett scenario
+                                                                  //  genom listor som är skrivna i program
         {
             Console.WriteLine(frågan);
 
@@ -261,7 +302,7 @@ namespace FiskFara
             }
         }
 
-        static void natt(int shelter, int hung, int hp, int heat, int time, int dagNum)
+        static void natt(int shelter, int hung, int hp, int heat, int time, int dagNum) // detta visar alla stats i spelet och säger vilken dag som är förbi
         {
             dagNum++;
             WriteLine("Dag " + dagNum + " är nu förbi.");
@@ -291,8 +332,11 @@ namespace FiskFara
                 hp = hp + 20;
             }
             WriteLine("Hälsa: " + hp);
-            WriteLine(time);
-            Console.ReadLine();
+            // WriteLine(time);
+            ReadLine();
+            Clear();
         }
+
+        // jag tänkte göra en metod där det står att man har dött // varje while loop ska läggas till med om hp != 0
     }
 }
